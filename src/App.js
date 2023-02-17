ab70ad9822fc997a52110637b4fb4fbe2746216f
Mikuz23 <mikyzturati@gmail.com>
2023-02-17 11:34:33 +0100

    WOW

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+import pic from "C:/Users/Studente/Documents/Classe4f/portfolio-4fti/src/download.jpg";
+function App(){
+ return <img src={pic} />
+}
+
+
+export default App;
